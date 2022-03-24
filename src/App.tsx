6c89287ba843bcf5c90f4ba626e89e3d1a184d95
Maxim Khmara <maxim.khmara@gearheart.io>
2022-03-24 14:41:40 +0200

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -14,37 +14,37 @@ function App() {
     // 'bottom-right',
   ]
 
-  const slidesSchema = ['collapsed', 'main', 'expanded']
-  slidesSchema.unshift('')
+  const sliderSchema = ['collapsed', 'main', 'expanded']
+  sliderSchema.unshift('')
 
   return (
     <>
       {positionList.map((position) => (
         <div key={position} className={`widget widget-${position}`}>
           <div className="slider" style={{ width: 175, height: 130 }}>
-            <div className="slide" data-slide={slidesSchema[1]}>
+            <div className="slide" data-slide={sliderSchema[1]}>
               <div className="slide-wrapper">
                 <h4>slide: collapsed</h4>
-                <button className="trigger" data-trigger={slidesSchema[2]}>
+                <button className="trigger" data-trigger={sliderSchema[2]}>
                   to main
                 </button>
               </div>
             </div>
-            <div className="slide" data-slide={slidesSchema[2]}>
+            <div className="slide" data-slide={sliderSchema[2]}>
               <div className="slide-wrapper">
                 <h4>slide: main</h4>
-                <button className="trigger" data-trigger={slidesSchema[1]}>
+                <button className="trigger" data-trigger={sliderSchema[1]}>
                   collapse x
                 </button>
-                <button className="trigger" data-trigger={slidesSchema[3]}>
+                <button className="trigger" data-trigger={sliderSchema[3]}>
                   expand {'>'}
                 </button>
               </div>
             </div>
-            <div className="slide" data-slide={slidesSchema[3]}>
+            <div className="slide" data-slide={sliderSchema[3]}>
               <div className="slide-wrapper">
                 <h4>slide: expanded</h4>
-                <button className="trigger" data-trigger={slidesSchema[2]}>
+                <button className="trigger" data-trigger={sliderSchema[2]}>
                   to main
                 </button>
               </div>
@@ -55,29 +55,29 @@ function App() {
 
       <div className={`widget widget-bottom-right`}>
         <div className="slider slider-direction-horizontal" style={{ width: 175, height: 130 }}>
-          <div className="slide" data-slide={slidesSchema[1]}>
+          <div className="slide" data-slide={sliderSchema[1]}>
             <div className="slide-wrapper">
               <h4>slide: collapsed</h4>
-              <button className="trigger" data-trigger={slidesSchema[2]}>
+              <button className="trigger" data-trigger={sliderSchema[2]}>
                 to main
               </button>
             </div>
           </div>
-          <div className="slide" data-slide={slidesSchema[2]}>
+          <div className="slide" data-slide={sliderSchema[2]}>
             <div className="slide-wrapper">
               <h4>slide: main</h4>
-              <button className="trigger" data-trigger={slidesSchema[1]}>
+              <button className="trigger" data-trigger={sliderSchema[1]}>
                 collapse x
               </button>
-              <button className="trigger" data-trigger={slidesSchema[3]}>
+              <button className="trigger" data-trigger={sliderSchema[3]}>
                 expand {'>'}
               </button>
             </div>
           </div>
-          <div className="slide" data-slide={slidesSchema[3]}>
+          <div className="slide" data-slide={sliderSchema[3]}>
             <div className="slide-wrapper">
               <h4>slide: expanded</h4>
-              <button className="trigger" data-trigger={slidesSchema[2]}>
+              <button className="trigger" data-trigger={sliderSchema[2]}>
                 to main
               </button>
             </div>
