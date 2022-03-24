91fe83fc4feec3dc1c78f4ff9072d8bef4e832a9
Maxim Khmara <maxim.khmara@gearheart.io>
2022-03-24 13:23:35 +0200

    base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useEffect} from "react";
+
+function behavior () {
+	const widgets = document.querySelectorAll( '.widget' )
+	;[...widgets].forEach( widget => slider(widget) )
+}
+
+function slider ( widget ) {
+	const triggerList = widget.querySelectorAll('.trigger')
+	;[...triggerList].forEach( triggerElem => {
+		triggerElem.addEventListener('click', (e) => {
+			e.preventDefault()
+			widget.dataset.slide = triggerElem.dataset.trigger;
+		})
+	} )
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	useEffect( () => {
+		behavior()
+	} )
+
+	const positionList = [
+		'top-left',
+		'top-right',
+		'bottom-left',
+		'bottom-right',
+	]
+
+	return positionList.map( position => (
+		<div key={position} className={`widget widget-${position}`}>
+			<div className="slider">
+				<div className="slide" data-slide='collapsed' >
+					<div>
+						<button className='trigger' data-trigger='main' >main</button>
+					</div>
+				</div>
+				<div className="slide" data-slide="main">
+					<div>
+						<button className='trigger' data-trigger='expanded'>expand ></button>
+						<button className='trigger' data-trigger='collapsed'>collapse x</button>
+					</div>
+				</div>
+				<div className="slide" data-slide="expanded">
+					<div>
+						<button className='trigger' data-trigger='main' >main</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	) )
 }
 
 export default App;
